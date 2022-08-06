b906ad7c65d7f026ddd1a6355b1854e64b084eba
Brian <bmatzel1@binghamton.edu>
2022-08-06 16:37:39 -0400

    note dictionary added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// self.arpeggiation_dict = {  "C": ("C", "E", "G", "B"), "C_SHARP": ("C_SHARP", "F", "G_SHARP", "C2"),
+//                                     "D": ("D", "F_SHARP", "A", "C_SHARP2"), "D_SHARP": ("D_SHARP", "G", "A_SHARP", "D2"),
+//                                     "E": ("E", "G_SHARP", "B", "D_SHARP2"), "F": ("F", "A", "C2", "E2"),
+//                                     "F_SHARP": ("F_SHARP", "A_SHARP", "C_SHARP2", "F2"), "G": ("G", "B", "D2", "F_SHARP2"),
+//                                     "G_SHARP": ("G_SHARP", "C2", "D_SHARP2", "G2"), "A": ("A", "C_SHARP2", "E2", "G_SHARP2"),
+//                                     "A_SHARP": ("A_SHARP", "D2", "F2", "A2"), "B": ("B", "D_SHARP2", "F_SHARP2", "A_SHARP2"),
+//                                     "C2": ("C2", "E2", "G2", "B2"),
+//                                 }
+//         # dictionary for each note's frequency
+//         self.frequency_dict = { "C" : 261.6256, "C_SHARP" : 277.1826, "D" : 293.6648, "D_SHARP" : 311.1270,
+//                                 "E" : 329.6276, "F" : 349.2282, "F_SHARP" : 369.9944, "G" : 391.9954,
+//                                 "G_SHARP" : 415.3047, "A" : 440.0, "A_SHARP" : 466.1638, "B" : 493.8833,
+//                                 "C2" : 523.2511, "C_SHARP2" : 554.3653, "D2" : 587.3295, "D_SHARP2" : 622.2540,
+//                                 "E2" : 659.2551, "F2" : 698.4565, "F_SHARP2" : 739.9888, "G2" : 783.9909,
+//                                 "G_SHARP2" : 830.6094, "A2" : 880.0000, "A_SHARP2" : 932.3275, "B2" : 987.7666
+//                             }
+// https://docs.rs/rodio/latest/rodio/
+
 fn main () {
     
 }
